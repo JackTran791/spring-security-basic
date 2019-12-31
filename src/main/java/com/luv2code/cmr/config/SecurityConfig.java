e98fcb4e1b1f0fc24ea40a117c9aefbb36723334
JackTran791 <congsonit@gmail.com>
2019-12-31 14:08:14 -0600

    -Add datasource config file

--- src/main/java/com/luv2code/cmr/config/SecurityConfig.java
+++ src/main/java/com/luv2code/cmr/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.luv2code.cmr.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 /**
  * @author Jack Tran
@@ -12,11 +15,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource securityDataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("john").password("{noop}test123").roles("EMPLOYEE").and()
-                .withUser("mary").password("{noop}test123").roles("MANAGER", "EMPLOYEE").and()
-                .withUser("susan"). password("{noop}test123").roles("ADMIN", "EMPLOYEE");
+//        auth.inMemoryAuthentication().withUser("john").password("{noop}test123").roles("EMPLOYEE").and()
+//                .withUser("mary").password("{noop}test123").roles("MANAGER", "EMPLOYEE").and()
+//                .withUser("susan"). password("{noop}test123").roles("ADMIN", "EMPLOYEE");
+
+        auth.jdbcAuthentication().dataSource(securityDataSource);
     }
 
     @Override
